da624c6fdb7fae6235dfb9a1516751c703f3bac3
CarlG96 <carl.gardiner1@hotmail.com>
2022-10-17 14:48:51 +0000

    added multiplecomponents to do with event handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,8 @@ import EventBinding from './components/EventBinding';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsChild from './components/MethodsAsPropsChild';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
@@ -19,7 +21,8 @@ function App() {
       {/* <EventBinding /> */}
       {/* <ConditionalRenderingFunctional connected={false}/> */}
       {/* <ConditionalRenderingClass /> */}
-      <NestingComponents />
+      {/* <NestingComponents /> */}
+      <MethodsAsPropsParent />
     </div>
   );
 }
